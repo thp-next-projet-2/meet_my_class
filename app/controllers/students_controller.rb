b53bdc903b00696877957b61b7943a2479d02953
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-05-21 23:06:30 +0200

    feature: invitation creates student and attendance

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,13 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show; end
+
+  def create
+    @klass_id = params[:klass_id]
+    @email = params[:student][:email]
+    @student = Student.create!(email: @email)
+    Attendance.create!(student_id: @student.id, klass_id: @klass_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :klasses do
-    get 'students/index'
-    get 'students/new'
-    get 'students/create'
-  end
+  # namespace :klasses do
+  #  get 'students/index'
+  #  get 'students/new'
+  #  get 'students/create'
+  # end
   devise_for :teachers
 
   # SEE: https://github.com/plataformatec/devise/wiki/How-To:-Email-only-sign-up
   devise_for :students, controllers: {
     confirmations: 'students/confirmations',
-    passwords: 'students/passwords'
+    passwords: 'students/passwords',
+    registrations: 'students/registrations'
   }
 
   # SEE: https://guides.rubyonrails.org/routing.html#shallow-nesting
