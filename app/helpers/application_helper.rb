b7ba53d413304e1a16629e415674f38ade34004b
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-05-24 18:49:10 +0200

    base bootstrap header/flash/main/footer framework

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def flash_class(level)
+    case level
+    when 'notice' then "alert alert-primary alert-info alert-dismissible fade show"
+    when 'success' then "alert alert-primary alert-success alert-dismissible fade show"
+    when 'warning' then "alert alert-primary alert-warning alert-dismissible fade show"
+    when 'alert' then "alert alert-primary alert-error alert-dismissible fade show"
+    end
+  end
 end
