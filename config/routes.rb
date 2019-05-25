3d0ce1eeb6e05f248c3d970486a117315a1b0f57
TheMindset <mulezig2014@outlook.fr>
2019-05-25 15:19:43 +0200

    #23: generate controller home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'home#index'
+
   devise_for :teachers
   devise_for :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
