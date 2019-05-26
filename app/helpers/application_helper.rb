e9687e8af072705768ac3aa8c561c419fa31e2e9
TheMindset <mulezig2014@outlook.fr>
2019-05-26 11:28:55 +0200

    #23: built bootstrap messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def bootstrap_flash_messages(type)
+    case type
+    when 'notice' then "alert-info"
+    when 'success' then "alert-success"
+    when 'error' then "alert-danger"
+    when 'alert' then "alert-warning"
+    end
+  end
 end
