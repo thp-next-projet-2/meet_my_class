aa6d899737b5cf045d11e03a5a47a8d89b4966b1
TheMindset <mulezig2014@outlook.fr>
2019-05-28 08:16:41 +0200

    #40: CRUD controller klass ==> ok

--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -1,14 +1,57 @@
 # frozen_string_literal: true
 
 class KlassesController < ApplicationController
-  before_action :authenticate_student!
+  before_action :set_klass, only: %i[edit update show destroy]
+  before_action :authenticate_teacher!, only: %i[new create edit update destroy]
 
   def index
-    @klasses = current_student.klasses
+    # @klasses = current_student.klasses
+    @klasses = Klass.all
   end
 
   def show
     @klass = Klass.find(params[:id])
     @questions = @klass.questions
   end
+
+  def new
+    @klass = Klass.new
+  end
+
+  def create
+    if current_teacher
+      @klass = Klass.create(klass_params)
+      current_teacher.id = @klass.teacher_id
+    else
+      redirect_to klasses_path, alert: "Un problème est servenu veuillez réessayer"
+    end
+    if @klass.save
+      redirect_to klasses_path, success: "La classe a bien été crée avec succès"
+    else
+      render 'new'
+    end
+  end
+
+  def edit; end
+
+  def update
+    @klass.update(klass_params)
+    redirect_to klasses_path, success: "La classe a bien été mise a jour avec succès"
+  end
+
+  def destroy
+    @klass.destroy
+    redirect_to klasses_path, success: "La classe a bien été supprimer"
+  end
+
+  private
+
+  def set_klass
+    @klass = Klass.find(params[:id])
+  end
+
+  def klass_params
+    params.require(:klass).permit(:title, :description, :teacher_id)
+  end
 end
+current_teacher.id = @klass.teacher_id
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,4 @@ Rails.application.routes.draw do
   devise_for :teachers
   devise_for :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root 'klasses#index'
 end
