bc98543520affb41e9376cfaf400026df033a0ee
NIcoZF <nico-7@hotmail.fr>
2019-05-28 15:07:30 +0200

    merge master: pb nav_bar x2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,4 @@
 # frozen_string_literal: true
 
 class StudentsController < ApplicationController
-<<<<<<< HEAD
-  def index; end
-
-  def show; end
-
-  def create; end
-=======
->>>>>>> master
 end
