8b318b4328fc3fbdaf0e761ab30d89d49b52c70a
TheMindset <mulezig2014@outlook.fr>
2019-05-31 16:45:34 +0200

    #54: some tests

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -29,6 +29,9 @@
 FactoryBot.define do
   factory :student do
     email { Faker::Internet.unique.safe_email }
-    password { Faker::Internet.password }
+    password { "123456" }
+    password_confirmation { "123456" }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
   end
 end
--- spec/models/progression_spec.rb
+++ spec/models/progression_spec.rb
@@ -20,6 +20,44 @@
 require 'rails_helper'
 
 RSpec.describe Progression, type: :model do
-  it { is_expected.to belong_to(:step) }
-  it { is_expected.to belong_to(:student) }
+  describe 'Database' do
+    it { is_expected.to have_db_column(:student_id).of_type(:integer) }
+    it { is_expected.to have_db_column(:step_id).of_type(:integer) }
+    it { is_expected.to have_db_column(:completed) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+  end
+
+  describe "Associations" do
+    let(:progression) { build(:progression) }
+
+    it { expect(progression).to belong_to(:student) }
+    it { expect(progression).to belong_to(:step) }
+  end
+
+  describe 'Factories' do
+    context 'with valid attributes' do
+      let!(:progression) { build(:progression) }
+
+      it { expect(progression.errors).to be_empty }
+
+      it "is valid with valid attributes" do
+        expect(progression).to be_valid
+      end
+    end
+
+    context 'with unvalid attributes' do
+      let(:progression_wrong_step) { build(:progression, :invalid_step) }
+
+      let(:progression_wrong_student) { build(:progression, :invalid_student) }
+
+      it "is not valid with unvalid user_id" do
+        expect(progression_wrong_step).not_to be_valid
+      end
+
+      it "is not valid with unvalid student_id" do
+        expect(progression_wrong_student).not_to be_valid
+      end
+    end
+  end
 end
