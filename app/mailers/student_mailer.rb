386a62756e7a685a301ed1cac4016112fc255940
TheMindset <mulezig2014@outlook.fr>
2019-06-03 16:02:09 +0200

    #67: disable welcome email

--- app/mailers/student_mailer.rb
+++ app/mailers/student_mailer.rb
@@ -3,16 +3,16 @@
 class StudentMailer < ApplicationMailer
   default from: 'no-reply@themeetmyclass.com'
 
-  def welcome_email(student)
-    # on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
-    @student = student
+  # def welcome_email(student)
+  #   # on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+  #   @student = student
 
-    # on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url = 'https://meet-my-class-staging.herokuapp.com/'
+  #   # on définit une variable @url qu'on utilisera dans la view d’e-mail
+  #   @url = 'https://meet-my-class-staging.herokuapp.com/'
 
-    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @student.email, subject: 'Bienvenue chez nous !')
-  end
+  #   # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+  #   mail(to: @student.email, subject: 'Bienvenue chez nous !')
+  # end
 
   def invitation_klass_mail(attendance)
     @attendance = attendance
--- app/models/student.rb
+++ app/models/student.rb
@@ -44,9 +44,9 @@ class Student < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :upvotes, dependent: :destroy
 
-  after_create :welcome_send
+  # after_create :welcome_send
 
-  def welcome_send
-    StudentMailer.welcome_email(self).deliver_now
-  end
+  # def welcome_send
+  #   StudentMailer.welcome_email(self).deliver_now
+  # end
 end
