dd6a972ab5a0b1bc53382e9184364174475dc4b7
NIcoZF <nico-7@hotmail.fr>
2019-06-04 15:05:41 +0200

    fix syntax lesson model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,5 +3,5 @@
 class Lesson < ApplicationRecord
   belongs_to :teacher, class_name: 'User', foreign_key: 'user_id', inverse_of: :lessons
   has_many :attendances, dependent: :destroy
-  has_many :students, class_name: 'User', through: attendances
+  has_many :students, class_name: 'User', through: :attendances
 end
