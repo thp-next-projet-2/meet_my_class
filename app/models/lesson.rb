5f8979ce716bc38cc42d905c0fb51975ea1a12ac
NIcoZF <nico-7@hotmail.fr>
2019-06-05 18:57:32 +0200

    add lessons steps relation in models

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Lesson < ApplicationRecord
-    has_many :lesson_steps, dependant: :destroy
-    has_many :steps, through: :lesson_steps
-    belongs_to :teacher, foreign_key: 'teacher_id', class_name: 'User'
+  has_many :lessons_steps, dependent: :destroy
+  has_many :steps, through: :lessons_steps
+  belongs_to :teacher, foreign_key: 'teacher_id', class_name: 'User', inverse_of: lessons
 end
