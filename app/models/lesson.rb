166b387b1bb69147a26733da91eea80a87b71bcf
NIcoZF <nico-7@hotmail.fr>
2019-06-05 18:22:49 +0200

    create join table lessons steps

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,5 @@
-# frozen_string_literal: true
-
 class Lesson < ApplicationRecord
+    has_many :lesson_steps, dependant: :destroy
+    has_many :steps, through: :lesson_steps
+    belongs_to :teacher, foreign_key: 'teacher_id', class_name: 'User'
 end
