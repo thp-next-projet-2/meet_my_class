36036287180d3f94b2d01a4782c1e0439daa6a2f
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-06-05 20:05:26 +0200

    fix progressions percentage in klasses#show

--- app/helpers/steps_helper.rb
+++ app/helpers/steps_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module StepsHelper
+  def progression_percentage(step)
+    if step.klass.attendances.count == 0
+      0
+    else
+      step.progressions.count*100/step.klass.attendances.count
+    end
+  end
 end
