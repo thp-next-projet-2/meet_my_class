38c5a74fde49037e5fa50636a5619e8e57f24088
NIcoZF <nico-7@hotmail.fr>
2019-06-06 14:48:48 +0200

    fix jointable syntax

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -19,5 +19,5 @@
 class Lesson < ApplicationRecord
   has_many :lessons_steps, dependent: :destroy
   has_many :steps, through: :lessons_steps
-  belongs_to :teacher, foreign_key: 'teacher_id', class_name: 'User', inverse_of: lessons
+  belongs_to :teacher, foreign_key: 'teacher_id', class_name: 'User', inverse_of: :lessons
 end
