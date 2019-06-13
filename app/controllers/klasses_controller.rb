915e7c14acdb634211ddaba864168eb3f1b0ca19
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-06-13 15:27:20 +0200

    add pundit policies

--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class KlassesController < ApplicationController
+  before_action :authenticate_student!
+  rescue_from Pundit::NotAuthorizedError, with: :redirect_unauthorized
+
   def index
     @klasses = current_student.klasses
     # @klasses = Klass.all
@@ -8,6 +11,7 @@ class KlassesController < ApplicationController
 
   def show
     @klass = Klass.find(params[:id])
+    authorize(@klass, :following?)
     # @questions = @klass.questions
 
     @questions = @klass.questions.sort_by(&:count_votes).reverse
@@ -23,5 +27,16 @@ class KlassesController < ApplicationController
 
     # for step
     @steps = @klass.steps
+  end
+
+  private
+
+  def redirect_unauthorized
+    flash[:alert] = "Vous n'êtes pas membre de cette classe"
+    redirect_to root_path
+  end
+
+  def pundit_user
+    current_student
   end
 end
