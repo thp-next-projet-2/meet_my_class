e1b7274bcee95495ca4b7c2a7f8c34b56dc8c709
TheMindset <mulezig2014@outlook.fr>
2019-07-09 11:06:48 +0200

    #1: integrate authenticate pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@
 
 class ApplicationController < ActionController::Base
   include Pundit
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password])
+  end
 end
